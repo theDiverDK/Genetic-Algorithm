a2d99514f8648014924f45f4d2aa8d1a2aa8bf5c
Søren Reinke <soren@reinke.dk>
2018-05-03 15:21:00 +0200

    WIP

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,6 @@
+import GeneticAlgorithm.StringGA
+
+if __name__ == '__main__':
+    ga = StringGA(20)
+
+    print(ga)
---
+++ GeneticAlgorithm/StringGA.py
@@ -0,0 +1,8 @@
+
+
+def StringGA:
+    def __init__(self, length):
+        self.length = length
+
+    def __str__(self):
+        return "StringGA of {self.length}"
